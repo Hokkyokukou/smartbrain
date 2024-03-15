2bcd0b0f33a4d14e06880c0e1fdafcb3c93411bf
Hokkyokukou <hokkyokukou27@gmail.com>
2024-03-15 11:57:32 +0100

    update components features

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React from 'react';
+import React, { Component} from 'react';
 
 //Components
 import Navigation from './components/Navigation/Navigation';
@@ -54,7 +54,7 @@ const returnClarifaiRequestOptions = (imageUrl) => {
     return requestOptions;
 };
 
-class App extends React.Component {
+class App extends Component {
     constructor() {
         super();
         this.state = {
@@ -70,19 +70,20 @@ class App extends React.Component {
                 entries: 0,
                 joined: '',
             },
-            loadUser: false
         };
     }
 
     loadUser = (data) => {
-        this.setState({user: {
-            id: data.id,
-            name: data.name,
-            email: data.email,
-            entries: data.entries,
-            joined: data.joined
-        }})
-    }
+        this.setState({
+            user: {
+                id: data.id,
+                name: data.name,
+                email: data.email,
+                entries: data.entries,
+                joined: data.joined,
+            },
+        });
+    };
 
     calculateFaceLocation = (data) => {
         const regions = data.outputs[0].data.regions[0];
@@ -122,7 +123,7 @@ class App extends React.Component {
         this.setState({ input: event.target.value });
     };
 
-    onButtonSubmit = () => {
+    onPictureSubmit = () => {
         this.setState({ imageUrl: this.state.input });
         //app.models.predict('face-detection', this.state.input)
         fetch(
@@ -132,9 +133,22 @@ class App extends React.Component {
             returnClarifaiRequestOptions(this.state.input)
         )
             .then((response) => response.json())
-            .then((result) => {
-                console.log(result);
-                this.displayFaceBox(this.calculateFaceLocation(result));
+            .then((response) => {
+                // console.log(result);
+                if (response) {
+                    fetch('http://localhost:3000/image', {
+                        method: 'put',
+                        headers: { 'Content-Type': 'application/json' },
+                        body: JSON.stringify({
+                            id: this.state.id,
+                        }),
+                    })
+                    .then((response) => response.json())
+                    .then(count => {
+                        this.setState(Object.assign(this.state.user, { entries: count}))
+                    });
+                }
+                this.displayFaceBox(this.calculateFaceLocation(response));
             });
     };
 
@@ -159,17 +173,26 @@ class App extends React.Component {
                 {route === 'home' ? (
                     <div>
                         <Logo />
-                        <Rank name={this.state.user.name} entries={this.state.user.entries} />
+                        <Rank
+                            name={this.state.user.name}
+                            entries={this.state.user.entries}
+                        />
                         <ImageLinkForm
                             onInputChange={this.onInputChange}
-                            onButtonSubmit={this.onButtonSubmit}
+                            onPictureSubmit={this.onPictureSubmit}
                         />
                         <FaceRecognition box={box} imageUrl={imageUrl} />
                     </div>
                 ) : route === 'signin' ? (
-                    <SignIn loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
+                    <SignIn
+                        loadUser={this.loadUser}
+                        onRouteChange={this.onRouteChange}
+                    />
                 ) : (
-                    <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
+                    <Register
+                        loadUser={this.loadUser}
+                        onRouteChange={this.onRouteChange}
+                    />
                 )}
             </div>
         );
--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './ImageLinkForm.css';
 
-const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
+const ImageLinkForm = ({ onInputChange, onPictureSubmit }) => {
     return (
         <div>
             <p className="f3">
@@ -18,7 +18,7 @@ const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
                     />
                     <button
                         className="w-30 grow f4 link ph3 pv2 div white bg-light-purple"
-                        onClick={onButtonSubmit}
+                        onClick={onPictureSubmit}
                     >
                         Detect
                     </button>

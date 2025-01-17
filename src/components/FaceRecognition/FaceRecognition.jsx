56f8871cfb3228b783bf50a4b98130edcfcd2b85
Hokkyokukou <hokkyokukou27@gmail.com>
2025-01-17 13:14:05 +0100

    fix: handle empty imageUrl and prevent empty src rendering
    - Added condition to check if `imageUrl` is present before rendering image
    - Prevented empty string from being passed to the `src` attribute
    - Ensured image is only rendered when valid `imageUrl` is provided

--- src/components/FaceRecognition/FaceRecognition.jsx
+++ src/components/FaceRecognition/FaceRecognition.jsx
@@ -4,22 +4,28 @@ const FaceRecognition = ({ imageUrl, box }) => {
     return (
         <div className="center ma">
             <div className="absolute mt2">
-                <img
-                    src={imageUrl}
-                    alt=""
-                    width="500px"
-                    height="auto"
-                    id="inputimage"
-                />
-                <div
-                    className="bounding-box"
-                    style={{
-                        top: box.topRow,
-                        right: box.rightCol,
-                        bottom: box.bottomRow,
-                        left: box.leftCol,
-                    }}
-                ></div>
+                {/* Condizione per evitare di rendere l'immagine con un src vuoto */}
+                {imageUrl && (
+                    <img
+                        src={imageUrl}
+                        alt=""
+                        width="500px"
+                        height="auto"
+                        id="inputimage"
+                    />
+                )}
+                {/* Renderizza la box solo se imageUrl è presente */}
+                {imageUrl && (
+                    <div
+                        className="bounding-box"
+                        style={{
+                            top: box.topRow,
+                            right: box.rightCol,
+                            bottom: box.bottomRow,
+                            left: box.leftCol,
+                        }}
+                    ></div>
+                )}
             </div>
         </div>
     );
